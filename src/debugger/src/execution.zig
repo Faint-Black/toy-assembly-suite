69049fef996f2157491083c66a2f00bf494b5b08
Cezar <fntblk@gmail.com>
2025-04-18 22:05:51 -0300

    neater log messages

--- src/debugger/src/execution.zig
+++ src/debugger/src/execution.zig
@@ -48,7 +48,7 @@ pub fn Run_Virtual_Machine(vm: *machine.VirtualMachine, flags: clap.Flags, heade
                 try vm.Syscall();
             },
             .STRIDE_LIT => {
-                // get char literal from following ROM byte
+                // set byte stride for indexing instructions
                 const stride: u8 = vm.rom[vm.program_counter + specs.bytelen.opcode];
                 if (flags.log_instruction_sideeffects) {
                     stdout.print("Before:\nByte stride = {}\n", .{vm.index_byte_stride}) catch unreachable;
@@ -99,132 +99,132 @@ pub fn Run_Virtual_Machine(vm: *machine.VirtualMachine, flags: clap.Flags, heade
                 try vm.Return_From_Subroutine();
             },
             .LDA_LIT => {
-                // get literal from following ROM bytes, then put it in the accumulator
+                // load literal into accumulator
                 const literal: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\":\nA = {}\n", .{ literal, vm.accumulator }) catch unreachable;
+                    stdout.print("Before loading literal 0x{X:0>8}:\nA = {}\n", .{ literal, vm.accumulator }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(literal, &vm.accumulator);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\":\nA = {}\n", .{ literal, vm.accumulator }) catch unreachable;
+                    stdout.print("After loading literal 0x{X:0>8}:\nA = {}\n", .{ literal, vm.accumulator }) catch unreachable;
                 }
             },
             .LDX_LIT => {
-                // get literal from following ROM bytes, then put it in the x index
+                // load literal into X index
                 const literal: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\":\nX = {}\n", .{ literal, vm.x_index }) catch unreachable;
+                    stdout.print("Before loading literal 0x{X:0>8}:\nX = {}\n", .{ literal, vm.x_index }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(literal, &vm.x_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\":\nX = {}\n", .{ literal, vm.x_index }) catch unreachable;
+                    stdout.print("After loading literal 0x{X:0>8}:\nX = {}\n", .{ literal, vm.x_index }) catch unreachable;
                 }
             },
             .LDY_LIT => {
-                // get literal from following ROM bytes, then put it in the y index
+                // load literal into Y index
                 const literal: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\":\nY = {}\n", .{ literal, vm.y_index }) catch unreachable;
+                    stdout.print("Before loading literal 0x{X:0>8}:\nY = {}\n", .{ literal, vm.y_index }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(literal, &vm.y_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\":\nY = {}\n", .{ literal, vm.y_index }) catch unreachable;
+                    stdout.print("After loading literal 0x{X:0>8}:\nY = {}\n", .{ literal, vm.y_index }) catch unreachable;
                 }
             },
             .LDA_ADDR => {
-                // get address from following ROM bytes, then fetch address contents from RAM and put it in the accumulator
+                // load address contents into accumulator
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 const address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\" from 0x{X:0>4}:\nA = {}\n", .{ address_contents, address, vm.accumulator }) catch unreachable;
+                    stdout.print("Before loading contents (0x{X:0>8}) from 0x{X:0>4}:\nA = {}\n", .{ address_contents, address, vm.accumulator }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(address_contents, &vm.accumulator);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\" from 0x{X:0>4}:\nA = {}\n", .{ address_contents, address, vm.accumulator }) catch unreachable;
+                    stdout.print("After loading contents (0x{X:0>8}) from 0x{X:0>4}:\nA = {}\n", .{ address_contents, address, vm.accumulator }) catch unreachable;
                 }
             },
             .LDX_ADDR => {
-                // get address from following ROM bytes, then fetch address contents from RAM and put it in the x index
+                // load address contents into X index
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 const address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\" from 0x{X:0>4}:\nX = {}\n", .{ address_contents, address, vm.x_index }) catch unreachable;
+                    stdout.print("Before loading (0x{X:0>8}) from 0x{X:0>4}:\nX = {}\n", .{ address_contents, address, vm.x_index }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(address_contents, &vm.x_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\" from 0x{X:0>4}:\nX = {}\n", .{ address_contents, address, vm.x_index }) catch unreachable;
+                    stdout.print("After loading (0x{X:0>8}) from 0x{X:0>4}:\nX = {}\n", .{ address_contents, address, vm.x_index }) catch unreachable;
                 }
             },
             .LDY_ADDR => {
-                // get address from following ROM bytes, then fetch address contents from RAM and put it in the y index
+                // load address contents into Y index
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 const address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before loading \"{}\" from 0x{X:0>4}:\nY = {}\n", .{ address_contents, address, vm.y_index }) catch unreachable;
+                    stdout.print("Before loading (0x{X:0>8}) from 0x{X:0>4}:\nY = {}\n", .{ address_contents, address, vm.y_index }) catch unreachable;
                 }
                 vm.Load_Value_Into_Reg(address_contents, &vm.y_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After loading \"{}\" from 0x{X:0>4}:\nY = {}\n", .{ address_contents, address, vm.y_index }) catch unreachable;
+                    stdout.print("After loading (0x{X:0>8}) from 0x{X:0>4}:\nY = {}\n", .{ address_contents, address, vm.y_index }) catch unreachable;
                 }
             },
             .LDA_X => {
                 // a = x
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering X to A:\nX = {}, A = {}\n", .{ vm.x_index, vm.accumulator }) catch unreachable;
+                    stdout.print("Before transfering X to A:\nX = 0x{X:0>8}, A = 0x{X:0>8}\n", .{ vm.x_index, vm.accumulator }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.accumulator, &vm.x_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering X to A:\nX = {}, A = {}\n", .{ vm.x_index, vm.accumulator }) catch unreachable;
+                    stdout.print("After transfering X to A:\nX = 0x{X:0>8}, A = 0x{X:0>8}\n", .{ vm.x_index, vm.accumulator }) catch unreachable;
                 }
             },
             .LDA_Y => {
                 // a = y
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering Y to A:\nY = {}, A = {}\n", .{ vm.y_index, vm.accumulator }) catch unreachable;
+                    stdout.print("Before transfering Y to A:\nY = 0x{X:0>8}, A = 0x{X:0>8}\n", .{ vm.y_index, vm.accumulator }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.accumulator, &vm.y_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering Y to A:\nY = {}, A = {}\n", .{ vm.y_index, vm.accumulator }) catch unreachable;
+                    stdout.print("After transfering Y to A:\nY = 0x{X:0>8}, A = 0x{X:0>8}\n", .{ vm.y_index, vm.accumulator }) catch unreachable;
                 }
             },
             .LDX_A => {
                 // x = a
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering A to X:\nA = {}, X = {}\n", .{ vm.accumulator, vm.x_index }) catch unreachable;
+                    stdout.print("Before transfering A to X:\nA = 0x{X:0>8}, X = 0x{X:0>8}\n", .{ vm.accumulator, vm.x_index }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.x_index, &vm.accumulator);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering A to X:\nA = {}, X = {}\n", .{ vm.accumulator, vm.x_index }) catch unreachable;
+                    stdout.print("After transfering A to X:\nA = 0x{X:0>8}, X = 0x{X:0>8}\n", .{ vm.accumulator, vm.x_index }) catch unreachable;
                 }
             },
             .LDX_Y => {
                 // x = y
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering Y to X:\nY = {}, X = {}\n", .{ vm.y_index, vm.x_index }) catch unreachable;
+                    stdout.print("Before transfering Y to X:\nY = 0x{X:0>8}, X = 0x{X:0>8}\n", .{ vm.y_index, vm.x_index }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.y_index, &vm.y_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering Y to X:\nY = {}, X = {}\n", .{ vm.y_index, vm.x_index }) catch unreachable;
+                    stdout.print("After transfering Y to X:\nY = 0x{X:0>8}, X = 0x{X:0>8}\n", .{ vm.y_index, vm.x_index }) catch unreachable;
                 }
             },
             .LDY_A => {
                 // y = a
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering A to Y:\nA = {}, Y = {}\n", .{ vm.accumulator, vm.y_index }) catch unreachable;
+                    stdout.print("Before transfering A to Y:\nA = 0x{X:0>8}, Y = 0x{X:0>8}\n", .{ vm.accumulator, vm.y_index }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.y_index, &vm.accumulator);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering A to Y:\nA = {}, Y = {}\n", .{ vm.accumulator, vm.y_index }) catch unreachable;
+                    stdout.print("After transfering A to Y:\nA = 0x{X:0>8}, Y = 0x{X:0>8}\n", .{ vm.accumulator, vm.y_index }) catch unreachable;
                 }
             },
             .LDY_X => {
                 // y = x
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("Before transfering X to Y:\nX = {}, Y = {}\n", .{ vm.x_index, vm.y_index }) catch unreachable;
+                    stdout.print("Before transfering X to Y:\nX = 0x{X:0>8}, Y = 0x{X:0>8}\n", .{ vm.x_index, vm.y_index }) catch unreachable;
                 }
                 vm.Transfer_Registers(&vm.y_index, &vm.x_index);
                 if (flags.log_instruction_sideeffects) {
-                    stdout.print("After transfering X to Y:\nX = {}, Y = {}\n", .{ vm.x_index, vm.y_index }) catch unreachable;
+                    stdout.print("After transfering X to Y:\nX = 0x{X:0>8}, Y = 0x{X:0>8}\n", .{ vm.x_index, vm.y_index }) catch unreachable;
                 }
             },
             .LDA_ADDR_X => {
@@ -417,78 +417,86 @@ pub fn Run_Virtual_Machine(vm: *machine.VirtualMachine, flags: clap.Flags, heade
                     vm.Jump_To_Address(address);
             },
             .ADD_LIT => {
-                // accumulator += literal
+                // accumulator += (literal + carry)
                 const literal: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u32);
                 vm.accumulator = vm.Add(vm.accumulator, literal);
             },
             .ADD_ADDR => {
-                // accumulator += address contents
+                // accumulator += (address contents + carry)
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 const address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
                 vm.accumulator = vm.Add(vm.accumulator, address_contents);
             },
             .ADD_X => {
-                // accumulator += X index value
+                // accumulator += (X index value + carry)
                 vm.accumulator = vm.Add(vm.accumulator, vm.x_index);
             },
             .ADD_Y => {
-                // accumulator += Y index value
+                // accumulator += (Y index value + carry)
                 vm.accumulator = vm.Add(vm.accumulator, vm.y_index);
             },
             .SUB_LIT => {
-                // accumulator -= literal
+                // accumulator -= (literal + carry - 1)
                 const literal: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u32);
                 vm.accumulator = vm.Subtract(vm.accumulator, literal);
             },
             .SUB_ADDR => {
-                // accumulator -= address contents
+                // accumulator -= (address contents + carry - 1)
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 const address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
                 vm.accumulator = vm.Subtract(vm.accumulator, address_contents);
             },
             .SUB_X => {
-                // accumulator -= X index value
+                // accumulator -= (X index value + carry - 1)
                 vm.accumulator = vm.Add(vm.accumulator, vm.x_index);
             },
             .SUB_Y => {
-                // accumulator -= Y index value
+                // accumulator -= (Y index value + carry - 1)
                 vm.accumulator = vm.Add(vm.accumulator, vm.x_index);
             },
             .INC_A => {
                 // accumulator += 1
+                vm.carry_flag = false;
                 vm.accumulator = vm.Add(vm.accumulator, 1);
             },
             .INC_X => {
                 // X index += 1
+                vm.carry_flag = false;
                 vm.x_index = vm.Add(vm.x_index, 1);
             },
             .INC_Y => {
                 // Y index += 1
+                vm.carry_flag = false;
                 vm.y_index = vm.Add(vm.y_index, 1);
             },
             .INC_ADDR => {
                 // contents of address += 1
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 var address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
+                vm.carry_flag = false;
                 address_contents = vm.Add(address_contents, 1);
                 machine.VirtualMachine.Write_Contents_Into_Memory_As(&vm.wram, address, @TypeOf(address_contents), address_contents);
             },
             .DEC_A => {
                 // accumulator -= 1
+                vm.carry_flag = true;
                 vm.accumulator = vm.Subtract(vm.accumulator, 1);
             },
             .DEC_X => {
                 // X index -= 1
+                vm.carry_flag = true;
                 vm.x_index = vm.Subtract(vm.x_index, 1);
             },
             .DEC_Y => {
                 // Y index -= 1
+                vm.carry_flag = true;
                 vm.y_index = vm.Subtract(vm.y_index, 1);
             },
             .DEC_ADDR => {
                 // contents of address -= 1
                 const address: u16 = machine.VirtualMachine.Read_Address_Contents_As(&vm.rom, vm.program_counter + specs.bytelen.opcode, u16);
                 var address_contents: u32 = machine.VirtualMachine.Read_Address_Contents_As(&vm.wram, address, u32);
+                vm.carry_flag = true;
                 address_contents = vm.Subtract(address_contents, 1);
                 machine.VirtualMachine.Write_Contents_Into_Memory_As(&vm.wram, address, @TypeOf(address_contents), address_contents);
             },
